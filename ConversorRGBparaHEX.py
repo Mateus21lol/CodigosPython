dd508be874bf8fd90577015213017d55a3b8e71f
Mateus21lol <58150931+Mateus21lol@users.noreply.github.com>
2021-02-16 21:07:21 -0300

    Add files via upload

---
+++ ConversorRGBparaHEX.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 20:35:31 2021
+
+@author: Mateus Nobre Silva Almeida
+"""
+
+
+
+
+print("HELLO: RGB")
+
+from colormap import rgb2hex
+
+
+
+Red = 16
+Green = 16
+Blue = 16
+
+result = rgb2hex(Red, Green, Blue)
+
+print(result)
+
